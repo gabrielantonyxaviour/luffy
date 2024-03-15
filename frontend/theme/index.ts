005d9d1c91c21af749cb0ba674ba18e14318ac95
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2024-03-15 21:47:38 +0000

    apply mui theme provider

---
+++ frontend/theme/index.ts
@@ -0,0 +1,12 @@
+'use client';
+
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#0A1620'
+    }
+  }
+});
