aa85511f3a6726bc15dbce919708ec6542850a2d
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-03-16 23:15:26 +0000

    luffy rewards set

--- blockend/contracts/LuffyRewards.sol
+++ blockend/contracts/LuffyRewards.sol
@@ -11,29 +11,32 @@ error InvalidGameweek(uint256 gameweek);
 error InvalidTokenAmount(uint256 _amount);
 error TokenNotApproved(uint256 _amount);
 error InadequateCrosschainFee(uint32 destinationDomain, uint256 requiredFee, uint256 sentFee);
+error NotAllowedCaller(bytes32 caller);
 
 contract LuffyRewards {
 
     address public owner;
     IMailbox public mailbox;
-    bytes32 public immutable REWARD_TOKEN;
+    string public  REWARD_TOKEN;
     IERC20 public immutable REWARD_TOKEN_ADDRESS;
-    mapping(address=>uint256) public playerToBetAmounts;
-    mapping(address=>uint256) public playerToRewardAmounts;
+    mapping(uint256=>uint256) public playerToBetAmounts;
+    mapping(uint256=>uint256) public playerToRewardAmounts;
+    mapping(uint256=>address)   public playerToAddress;
     uint256 public gameWeekCounter;
+    bytes32 public protocolAddress;
     
-
-    constructor(IMailbox _mailbox, string memory _rewardToken, IERC20 _rewardTokenAddress)
+    constructor(IMailbox _mailbox, string memory _rewardToken, IERC20 _rewardTokenAddress, bytes32 _protocolAddress)
     {
         mailbox = _mailbox;
-        REWARD_TOKEN = keccak256(abi.encodePacked(_rewardToken));
+        REWARD_TOKEN = _rewardToken;
         REWARD_TOKEN_ADDRESS = _rewardTokenAddress;
         gameWeekCounter=0;
         owner=msg.sender;
+        protocolAddress = _protocolAddress;
     }
 
-    event BetPlaced(uint256 indexed gameWeek, address indexed player, uint256 tokenAmount);
-    event RewardsClaimed(uint256 indexed gameWeek, address indexed player, uint256 tokenAmount);
+    event BetPlaced(uint256 indexed gameWeek, uint256 indexed nullifierHash, uint256 tokenAmount);
+    event RewardsClaimed(uint256 indexed gameWeek, uint256 indexed nullifierHash, uint256 tokenAmount);
 
     modifier onlyOwner {
         if(msg.sender != owner) revert NotOwner(msg.sender);
@@ -45,7 +48,7 @@ contract LuffyRewards {
         _;
     }
 
-    function betAmount(uint256 _gameWeek, uint256 _amount) public {
+    function betAmount(uint256 _gameWeek, uint256 _nullifier, uint256 _amount, bool isChilizOrApeCoin) public payable {
         if(_amount > 0) revert InvalidTokenAmount(_amount);
         if(_gameWeek!=gameWeekCounter+1) revert InvalidGameweek(_gameWeek);
         if(address(REWARD_TOKEN_ADDRESS) != address(0))
@@ -55,58 +58,61 @@ contract LuffyRewards {
         } else{
             payable(address(this)).transfer(_amount);
         }
-        playerToBetAmounts[msg.sender] += _amount;
+        playerToBetAmounts[_nullifier] += _amount;
+        playerToAddress[_nullifier] = msg.sender;
         
-        emit BetPlaced(_gameWeek, msg.sender, _amount);
+        emit BetPlaced(_gameWeek, _nullifier, _amount);
     }
 
-    function _claimRewards(uint256 _gameWeek, uint256 _amount, address receiver) internal returns(bool)
+    function _claimRewards(uint256 _gameWeek, uint256 _nulliferHash, uint256 _amount) internal returns(bool)
     {
-        if(_gameWeek>gameWeekCounter) gameWeekCounter = _gameWeek; 
-        playerToRewardAmounts[receiver] += _amount;
+        if(_gameWeek > gameWeekCounter) gameWeekCounter = _gameWeek; 
+        playerToRewardAmounts[_nulliferHash] += _amount + 1 ether;
         if(address(REWARD_TOKEN_ADDRESS) != address(0))
         {
-            REWARD_TOKEN_ADDRESS.transfer(receiver, _amount);
+            REWARD_TOKEN_ADDRESS.transfer(playerToAddress[_nulliferHash], _amount + 1 ether);
         } else{
-            payable(receiver).transfer(_amount);
+            payable(playerToAddress[_nulliferHash]).transfer(_amount + 1 ether);
         }
-        emit RewardsClaimed(_gameWeek, receiver, _amount);
+        emit RewardsClaimed(_gameWeek, _nulliferHash, _amount);
         return true;
     }
+
+    function handle(uint32 _origin, bytes32 _sender, bytes calldata _message) external payable onlyMailbox{
+        if(_sender != protocolAddress) revert NotAllowedCaller(_sender);
+        (uint256 _gameWeek, uint256 _nullifierHash, uint256 _amount) = abi.decode(_message, (uint256, uint256, uint256));
+        _claimRewards(_gameWeek, _nullifierHash, _amount);
+    }
+
     
 
 
-    event SentPing(bytes32 indexed messageId, uint32 indexed destinationChain, address indexed destinationAddress, uint256 fee, bytes message);
-    event ReceivedPong(bytes32 indexed messageId, uint32 indexed originChain, address indexed senderAddress, string message); 
-    function pingHyperlane(uint32 destinationDomain, bytes32 recepientAddress, string memory message) public payable{
+    // // Hyperlane Playground
+    // event SentPing(bytes32 indexed messageId, uint32 indexed destinationChain, address indexed destinationAddress, uint256 fee, bytes message);
+    // event ReceivedPong(bytes32 indexed messageId, uint32 indexed originChain, address indexed senderAddress, string message); 
+    // function pingHyperlane(uint32 destinationDomain, bytes32 recepientAddress, string memory message) public payable{
 
-        uint256 _requiredFee = mailbox.quoteDispatch(destinationDomain, recepientAddress, bytes(message));
-        if(msg.value < _requiredFee) revert InadequateCrosschainFee(destinationDomain, _requiredFee, msg.value);
+    //     uint256 _requiredFee = mailbox.quoteDispatch(destinationDomain, recepientAddress, bytes(message));
+    //     if(msg.value < _requiredFee) revert InadequateCrosschainFee(destinationDomain, _requiredFee, msg.value);
 
-               bytes32 messageId = mailbox.dispatch{value: msg.value}(destinationDomain,recepientAddress, abi.encode(message));
-        emit SentPing(messageId, destinationDomain, bytes32ToAddress(recepientAddress), msg.value, abi.encode(message));
-    }
+    //            bytes32 messageId = mailbox.dispatch{value: msg.value}(destinationDomain,recepientAddress, abi.encode(message));
+    //     emit SentPing(messageId, destinationDomain, bytes32ToAddress(recepientAddress), msg.value, abi.encode(message));
+    // }
 
-    function handle(uint32 _origin,bytes32 _sender,bytes calldata _message) external payable onlyMailbox{
-        _pongHyperlane(_origin, _sender, bytes32(0), _message);
-    }
+    // function handle(uint32 _origin,bytes32 _sender,bytes calldata _message) external payable onlyMailbox{
+    //     _pongHyperlane(_origin, _sender, bytes32(0), _message);
+    // }
 
-    function _pongHyperlane(uint32 originDomain, bytes32 senderAddress, bytes32 messageId, bytes memory message) internal{
-        emit ReceivedPong(messageId, originDomain, bytes32ToAddress(senderAddress), abi.decode(message, (string)));
-    }
+    // function _pongHyperlane(uint32 originDomain, bytes32 senderAddress, bytes32 messageId, bytes memory message) internal{
+    //     emit ReceivedPong(messageId, originDomain, bytes32ToAddress(senderAddress), abi.decode(message, (string)));
+    // }
 
-    function bytes32ToAddress(bytes32 _buf) internal pure returns (address) {
-        return address(uint160(uint256(_buf)));
-    }
+    // function bytes32ToAddress(bytes32 _buf) internal pure returns (address) {
+    //     return address(uint160(uint256(_buf)));
+    // }
 
-    function getQuote(uint32 destinationDomain, bytes32 recepientAddress, string memory message) public view returns(uint256){
-        return mailbox.quoteDispatch(destinationDomain, recepientAddress, bytes(message));
-    }
+    // function getQuote(uint32 destinationDomain, bytes32 recepientAddress, string memory message) public view returns(uint256){
+    //     return mailbox.quoteDispatch(destinationDomain, recepientAddress, bytes(message));
+    // }
 
 }
-
-// pays some CHILIZ to take part in the betting
-
-
-
-// if funded, the contract will pay out the winner from cross chain transaction
