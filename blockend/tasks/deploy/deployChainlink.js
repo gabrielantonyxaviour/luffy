4a2bf81b46df5bf4767ecbfc849db549265ab51b
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-03-16 18:42:35 +0000

    add new reqs chainlink

---
+++ blockend/tasks/deploy/deployChainlink.js
@@ -0,0 +1,76 @@
+const { networks } = require("../../networks");
+
+task("deploy-chainlink", "Deploys the TestingChainlink contract")
+  .addOptionalParam(
+    "verify",
+    "Set to true to verify contract",
+    false,
+    types.boolean
+  )
+  .setAction(async (taskArgs) => {
+    console.log(`Deploying TestingChainlink contract to ${network.name}`);
+
+    console.log("\n__Compiling Contracts__");
+    await run("compile");
+    const args = [""];
+
+    const testingContractFactory = await ethers.getContractFactory(
+      "TestingChainlink"
+    );
+    const testingContract = await testingContractFactory.deploy(...args);
+
+    console.log(
+      `\nWaiting ${
+        networks[network.name].confirmations
+      } blocks for transaction ${
+        testingContract.deployTransaction.hash
+      } to be confirmed...`
+    );
+
+    await testingContract.deployTransaction.wait(
+      networks[network.name].confirmations
+    );
+
+    console.log(
+      "\nDeployed TestingChainlink contract to:",
+      testingContract.address
+    );
+
+    if (network.name === "localFunctionsTestnet") {
+      return;
+    }
+
+    const verifyContract = taskArgs.verify;
+    if (
+      network.name !== "localFunctionsTestnet" &&
+      verifyContract &&
+      !!networks[network.name].verifyApiKey &&
+      networks[network.name].verifyApiKey !== "UNSET"
+    ) {
+      try {
+        console.log("\nVerifying contract...");
+        await run("verify:verify", {
+          address: testingContract.address,
+          constructorArguments: args,
+        });
+        console.log("Contract verified");
+      } catch (error) {
+        if (!error.message.includes("Already Verified")) {
+          console.log(
+            "Error verifying contract.  Ensure you are waiting for enough confirmation blocks, delete the build folder and try again."
+          );
+          console.log(error);
+        } else {
+          console.log("Contract already verified");
+        }
+      }
+    } else if (verifyContract && network.name !== "localFunctionsTestnet") {
+      console.log(
+        "\nPOLYGONSCAN_API_KEY, ETHERSCAN_API_KEY or FUJI_SNOWTRACE_API_KEY is missing. Skipping contract verification..."
+      );
+    }
+
+    console.log(
+      `\n TestingChainlink contract deployed to ${testingContract.address} on ${network.name}`
+    );
+  });
