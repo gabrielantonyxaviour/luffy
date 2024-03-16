46e5b77435a64e8b2166d43e0fbf8c79202933e4
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-03-16 03:40:05 +0000

    add init contracts

---
+++ blockend/contracts/LuffyProtocol.sol
@@ -0,0 +1,8 @@
+
+
+
+// register squad for a game (upload merkle root) 
+
+// upload points by using chainlink functions and chainlink automations
+
+// verify zk proof and update points to the user on chain
---
+++ blockend/contracts/LuffyRewards.sol
@@ -0,0 +1,8 @@
+
+
+
+// pays some CHILIZ to take part in the betting
+
+
+
+// if funded, the contract will pay out the winner from cross chain transaction
