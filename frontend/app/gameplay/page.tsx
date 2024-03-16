ebeb92d39a46cbd42348adea5937abeafde9ec43
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2024-03-16 15:07:37 +0000

    add generate squad button

--- frontend/app/gameplay/page.tsx
+++ frontend/app/gameplay/page.tsx
@@ -1,14 +1,37 @@
-import { Box, Container, Typography } from '@mui/material';
+'use client';
+
+import { Container, Typography, Box, Button, Stack } from '@mui/material';
 import { Pitch } from '@/components';
+import { useGeneralContext } from '@/contexts';
 
 export default function GamePlay() {
+  const { setSquadGenerated } = useGeneralContext();
+
   return (
     <Box>
       <Container maxWidth='lg' sx={{ marginY: 3 }}>
         <Typography variant='h4' marginBottom={3}>
           Game Play
         </Typography>
-        <Pitch />
+        <Stack direction='row' alignItems='flex-start' justifyItems='center'>
+          <Pitch />
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '100%'
+            }}
+          >
+            <Button
+              variant='outlined'
+              color='warning'
+              onClick={() => setSquadGenerated(true)}
+            >
+              Generate Squad
+            </Button>
+          </Box>
+        </Stack>
       </Container>
     </Box>
   );
