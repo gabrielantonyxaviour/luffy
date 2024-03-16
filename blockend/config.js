a40d89df4dad6343188cb79cef1129c2befd1763
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-03-16 18:09:27 +0000

    add config and api key

--- blockend/config.js
+++ blockend/config.js
@@ -15,7 +15,7 @@ const requestConfig = {
   // Code language (only JavaScript is currently supported)
   codeLanguage: CodeLanguage.JavaScript,
   // Expected type of the returned value
-  expectedReturnType: ReturnType.string,
+  expectedReturnType: ReturnType.bytes,
 };
 
 module.exports = requestConfig;
